754c57bc0e08434bcc0780842a0aaccb9f56a91a
cyjadela <adelacho@naver.com>
2022-05-15 17:22:18 +0900

    feat : 내 정보 수정, 기록 작성, 기록 수정 화면

---
+++ app/src/main/java/com/example/spectory/SettingFragment.kt
@@ -0,0 +1,24 @@
+package com.example.spectory
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.spectory.databinding.FragmentArchivingBinding
+import com.example.spectory.databinding.FragmentSettingBinding
+
+class SettingFragment: Fragment() {
+    lateinit var binding : FragmentSettingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingBinding.inflate(inflater,container,false)
+
+        return binding.root
+    }
+
+}
