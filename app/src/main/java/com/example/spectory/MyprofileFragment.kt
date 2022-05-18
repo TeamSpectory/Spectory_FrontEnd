89040a98feb8886767e2cd781fa94052340329b5
cyjadela <adelacho@naver.com>
2022-05-18 17:44:12 +0900

    feat: MyProfile Fragment 화면 이동 구현

--- app/src/main/java/com/example/spectory/MyprofileFragment.kt
+++ app/src/main/java/com/example/spectory/MyprofileFragment.kt
@@ -1,9 +1,11 @@
 package com.example.spectory
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.spectory.databinding.FragmentMyprofileBinding
 
@@ -17,6 +19,17 @@ class MyprofileFragment: Fragment() {
     ): View? {
         binding= FragmentMyprofileBinding.inflate(inflater, container, false)
 
+        //연필 모양 클릭 시 내정보 수정 화면 열기 (SettingFragment)
+        binding.myprofileEditBtn.setOnClickListener {
+            Toast.makeText(this.context,"클릭됨",Toast.LENGTH_SHORT).show()
+            changeSettingFragment()
+        }
         return binding.root
+    }
+
+
+    private fun changeSettingFragment(){
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, SettingFragment()).commit()
     }
 }
