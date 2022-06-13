9ca304f11376e43935e53e2edf3d736251cc0c85
cyjadela <adelacho@naver.com>
2022-06-13 16:51:15 +0900

    feat : 글쓰기 화면_xml 수정

--- app/src/main/java/com/example/spectory/WriteActivity.kt
+++ app/src/main/java/com/example/spectory/WriteActivity.kt
@@ -8,6 +8,7 @@ import com.example.spectory.databinding.ActivityWriteBinding
 class WriteActivity: AppCompatActivity() {
 
     lateinit var binding: ActivityWriteBinding
+    private var star: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +25,56 @@ class WriteActivity: AppCompatActivity() {
             closeWriteActivity()
         }
 
+        star()
         setContentView(binding.root)
     }
 
     private fun closeWriteActivity(){
         finish()
     }
+
+    //별 눌렀을 때 사진 변경
+    private fun star(){
+        binding.writeStar1.setOnClickListener {
+            star = 1
+            binding.writeStar1.setImageResource(R.drawable.star_full)
+            binding.writeStar2.setImageResource(R.drawable.ic_star_)
+            binding.writeStar3.setImageResource(R.drawable.ic_star_)
+            binding.writeStar4.setImageResource(R.drawable.ic_star_)
+            binding.writeStar5.setImageResource(R.drawable.ic_star_)
+        }
+        binding.writeStar2.setOnClickListener {
+            star = 2
+            binding.writeStar1.setImageResource(R.drawable.star_full)
+            binding.writeStar2.setImageResource(R.drawable.star_full)
+            binding.writeStar3.setImageResource(R.drawable.ic_star_)
+            binding.writeStar4.setImageResource(R.drawable.ic_star_)
+            binding.writeStar5.setImageResource(R.drawable.ic_star_)
+        }
+        binding.writeStar3.setOnClickListener {
+            star = 3
+            binding.writeStar1.setImageResource(R.drawable.star_full)
+            binding.writeStar2.setImageResource(R.drawable.star_full)
+            binding.writeStar3.setImageResource(R.drawable.star_full)
+            binding.writeStar4.setImageResource(R.drawable.ic_star_)
+            binding.writeStar5.setImageResource(R.drawable.ic_star_)
+        }
+        binding.writeStar4.setOnClickListener {
+            star = 4
+            binding.writeStar1.setImageResource(R.drawable.star_full)
+            binding.writeStar2.setImageResource(R.drawable.star_full)
+            binding.writeStar3.setImageResource(R.drawable.star_full)
+            binding.writeStar4.setImageResource(R.drawable.star_full)
+            binding.writeStar5.setImageResource(R.drawable.ic_star_)
+        }
+        binding.writeStar5.setOnClickListener {
+            star = 5
+            binding.writeStar1.setImageResource(R.drawable.star_full)
+            binding.writeStar2.setImageResource(R.drawable.star_full)
+            binding.writeStar3.setImageResource(R.drawable.star_full)
+            binding.writeStar4.setImageResource(R.drawable.star_full)
+            binding.writeStar5.setImageResource(R.drawable.star_full)
+        }
+    }
+
 }
