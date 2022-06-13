572fe50665a8f0cf7b60fe17f9994e18a6b00b24
cyjadela <adelacho@naver.com>
2022-06-13 16:30:49 +0900

    feat : 내프로필 수정 서버 연동 구현

--- app/src/main/java/com/example/spectory/SettingFragment.kt
+++ app/src/main/java/com/example/spectory/SettingFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.spectory.databinding.FragmentArchivingBinding
 import com.example.spectory.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment() {
+class SettingFragment : Fragment(), ProfileView {
     lateinit var binding: FragmentSettingBinding
 
     override fun onCreateView(
@@ -17,8 +18,10 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
+        myprofile()
 
         binding.settingOkBtn.setOnClickListener {
+            //수정 완료 후 서버에 post 보내는 코드 짜야 함
             changeMyProfileFragment()
         }
 
@@ -28,6 +31,35 @@ class SettingFragment : Fragment() {
     private fun changeMyProfileFragment(){
         (context as MainActivity).supportFragmentManager.beginTransaction()
             .replace(R.id.main_frm, MyprofileFragment()).commit()
+    }
+
+    private fun getUserIdx(): Int{
+        val spf = this.activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+
+        return spf!!.getInt("userIdx",0)
+    }
+
+    private fun myprofile() {
+        val authService = AuthService()
+        authService.setProfileView(this)
+
+        //userIdx를 넣어줘야함 !! -> jwt 사용하기
+        authService.myprofile(getUserIdx())
+    }
+
+    //data에 있는 값들로 text 바꾸기
+    override fun onProfileSuccess(status: Int, data: Data) {
+        when(status){
+            200 -> {
+                binding.settingName.hint = data.nickname
+                binding.settingId.hint = data.id
+                binding.settingDate.hint = data.created_date
+            }
+        }
+    }
+
+    override fun onProfileFailure() {
+
     }
 
 }
